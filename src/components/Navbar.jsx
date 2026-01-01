190ca2e0edeb91f409424b4f8c596a8c2012c493
ratul43 <kmratul971@gmail.com>
2026-01-01 17:33:47 +0600

    Navbar component done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
-
+import { Link } from "react-router";
+import bookTruck from "../assets/rsz_1booktruck.png"
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative">
+    <nav className="flex items-center h-20 px-7 justify-between border-b border-gray-300 bg-white relative">
       {/* Logo */}
-      <a href="https://prebuiltui.com">
+      <Link className="flex items-center" to="/">
         <img
-          className="h-9"
-          src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/dummyLogo/dummyLogoColored.svg"
+          className="h-24 pt-4 object-center hidden md:block"
+          src={bookTruck}
           alt="Logo"
         />
-      </a>
+        <h1 className="font-extrabold hidden md:block ubuntu-bold-italic">BookCourier</h1>
+      </Link>
 
       {/* Mobile Menu Button */}
       <button
@@ -47,51 +49,20 @@ const Navbar = () => {
         <a href="#">About</a>
         <a href="#">Contact</a>
 
-        {/* Search */}
-        <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
-          <input
-            className="py-1.5 bg-transparent outline-none placeholder-gray-500"
-            type="text"
-            placeholder="Search products"
-          />
-          <svg width={16} height={16} viewBox="0 0 16 16" fill="none">
-            <path
-              d="M10.836 10.615 15 14.695"
-              stroke="#7A7B7D"
-              strokeWidth="1.2"
-              strokeLinecap="round"
-            />
-            <path
-              d="M9.141 11.738c2.729-1.136 4.001-4.224 2.841-6.898S7.67.921 4.942 2.057C2.211 3.193.94 6.281 2.1 8.955s4.312 3.92 7.041 2.783"
-              stroke="#7A7B7D"
-              strokeWidth="1.2"
-              strokeLinecap="round"
-            />
-          </svg>
         </div>
 
-        {/* Cart */}
-        <div className="relative cursor-pointer">
-          <svg width={18} height={18} viewBox="0 0 14 14" fill="none">
-            <path
-              d="M.583.583h2.333l1.564 7.81a1.17 1.17 0 0 0 1.166.94h5.67a1.17 1.17 0 0 0 1.167-.94l.933-4.893H3.5"
-              stroke="#615fff"
-              strokeLinecap="round"
-            />
-          </svg>
-          <span className="absolute -top-2 -right-3 text-xs text-white bg-indigo-500 w-[18px] h-[18px] rounded-full flex items-center justify-center">
-            3
-          </span>
-        </div>
-
-        <button className="px-8 py-2 bg-indigo-500 hover:bg-indigo-600 text-white rounded-full">
+       <div className="space-x-2">
+        
+        <button className="px-8 py-2 cursor-pointer bg-indigo-500 pointer hover:bg-indigo-600 text-white rounded-full">
           Login
         </button>
 
-        <button className="px-8 py-2 bg-indigo-500 hover:bg-indigo-600 text-white rounded-full">
+        <button className="px-8 py-2 cursor-pointer bg-indigo-500 hover:bg-indigo-600 text-white rounded-full">
           Register
         </button>
-      </div>
+       </div>
+
+     
     </nav>
   );
 };
