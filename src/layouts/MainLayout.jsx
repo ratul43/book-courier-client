883d60e714f0556bdca42989cadc94f736f23c9f
ratul43 <kmratul971@gmail.com>
2026-01-02 12:04:38 +0600

    footer component done

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 const MainLayout = () => {
     return (
         <div>
             <Navbar></Navbar>
+            <Footer></Footer>
         </div>
     );
 };
